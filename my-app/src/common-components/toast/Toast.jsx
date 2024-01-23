a4c8a7b8f9aee87b9394fe69a52a75338cf41fd0
fatima.batool.4416 <fatima.batool.4416@gmail.com>
2024-01-23 11:53:24 +0500

    successToast

--- my-app/src/common-components/toast/Toast.jsx
+++ my-app/src/common-components/toast/Toast.jsx
@@ -4,24 +4,11 @@ import './ToastStyles.css';
 const Toast = ({
   showSuccess, setShowSuccess
 }) => {
-  // const [showSuccess, setShowSuccess] = useState(false);
-  const [showWarning, setShowWarning] = useState(false);
-  const [showDanger, setShowDanger] = useState(false);
   const showToast = (type) => {
     switch (type) {
       case 'success':
         setShowSuccess(true);
         setTimeout(() => setShowSuccess(false), 3000);
-        break;
-      case 'warning':
-        setShowWarning(true);
-        setTimeout(() => setShowWarning(false), 3000);
-        break;
-      case 'danger':
-        setShowDanger(true);
-        setTimeout(() => setShowDanger(false), 3000);
-        break;
-      default:
         break;
     }
   };
@@ -33,22 +20,6 @@ const Toast = ({
             Successfully </p>
         </div>
       )}
-      {showWarning && (
-        <div className="toast warning">
-          <p>Warning <br /> Message</p>
-        </div>
-      )}
-      {showDanger && (
-        <div className="toast danger">
-          <p>Danger <br /> Message</p>
-        </div>
-      )}
-
-      {/* <div className="buttons-div">
-        <button onClick={() => showToast('success')}>Show Success Toast</button>
-        <button onClick={() => showToast('warning')}>Show Warning Toast</button>
-        <button onClick={() => showToast('danger')}>Show Danger Toast</button>
-      </div> */}
     </div>
   );
 };
